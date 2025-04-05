add4c2185120e3f159fa7210d4f2b3dbd44616b6
Simon Waloschek <waloschek@pm.me>
2025-04-06 00:42:59 +0200

    Remove unused dependencies from package.json and bun.lock; simplify App component by removing Router and directly rendering Demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import type { Component } from 'solid-js';
-import { Router, Route } from "@solidjs/router";
 import Demo from './pages/Demo';
 
 const App: Component = () => {
   return (
-    <Router>
-      <Route path="/" component={Demo} />
-      {/* Add other routes here */}
-
-    </Router>
+    <Demo />
   );
 };
 
